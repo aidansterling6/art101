3c4a5b3546eca7f37ff9b841c21deb39b16438ad
aidansterling6 <aidansterling6@gmail.com>
2022-11-07 10:49:31 -0800

    lab11 taskx

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -11,6 +11,23 @@ resultsButton.click(function(){
   resultsButton.parent().toggleClass("special");
 });
 
+var resultsButton2 = $("<button class='ChangeColor'>");
+resultsButton2.html("click me");
+$("#results").append("<br>");
+$("#results").append(resultsButton2);
+var resultsButton3 = $("<button>");
+resultsButton3.html("click me");
+$("#results").append("<br>");
+$("#results").append(resultsButton3);
+resultsButton2.click(function(){
+  resultsButton2.toggleClass("ChangeColor");
+  resultsButton3.toggleClass("ChangeColor");
+});
+resultsButton3.click(function(){
+  resultsButton2.toggleClass("ChangeColor");
+  resultsButton3.toggleClass("ChangeColor");
+});
+
 var challengesButton = $("<button>");
 challengesButton.html("challenges");
 $("#challenges").append("<br>");
