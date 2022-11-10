f7780d1104433a98cc7f03b7786ba9230ae6acc7
aidansterling6 <aidansterling6@gmail.com>
2022-11-09 17:23:37 -0800

    lab12 2

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -6,18 +6,8 @@
 function sortingHat(str) {
 	var length = str.length;
 	var mod = length%4;
-	if (mod==0) {
-		return "Gryffindor";
-	}
-	else if (mod==1) {
-		return "Ravenclaw";
-	}
-	else if (mod==2) {
-		return "Slytherin";
-	}
-	else if (mod==3) {
-		return "Hufflepuff";
-	}
+	var houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
+	return houses[mod];
 }
 
 var button=$("#button");
@@ -26,6 +16,21 @@ button.click(function() {
 	var name=input.val();
   console.log(name);
 	var house=sortingHat(name);
-	var paragraphOutput=$("<p>The Sorting Hat has sorted you into " + house + "</p>");
-	$("#output").append(paragraphOutput);
+	var paragraphOutput2=$("<p>The Sorting Hat has sorted you into " + house + "</p>");
+	$("#output").append(paragraphOutput2);
+	var paragraphOutput1;
+	if(house == "Gryffindor"){
+		paragraphOutput1=$("<p>Gryffindor's are brave and tend to act first and think later.</p>");
+		$("#output").append(paragraphOutput1);
+	} else if(house == "Ravenclaw"){
+		paragraphOutput1=$("<p>Ravenclaw's are smart, wise, and analyse everything.</p>");
+		$("#output").append(paragraphOutput1);
+	} else if(house == "Slytherin"){
+		paragraphOutput1=$("<p>Slytherin's are ambitious, shrewd, and often have a dark sense of humor.</p>");
+		$("#output").append(paragraphOutput1);
+	} else if(house == "Hufflepuff"){
+		paragraphOutput1=$("<p>Hufflepuff's are humble, kind, and hardworking.</p>");
+		$("#output").append(paragraphOutput1);
+	}
+
 });
