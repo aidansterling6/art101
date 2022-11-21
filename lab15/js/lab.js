debbb7b674162872ecc64c3257e7268be7b2171f
aidansterling6 <aidansterling6@gmail.com>
2022-11-21 12:52:13 -0800

    taskX

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -5,20 +5,44 @@
 */
 
 var info = null;
-
+var dificulty = 2;
 function getData() {
      $.ajax({
           url: "https://jservice.io/api/random",
           data: {
-               count: 4
+               count: dificulty
           },
           type: "GET",
           dataType: "json",
           success: function(data) {
                info = data;
-               console.log(data);
-               var temp = "question: " + data[0].question + "<br><br>answer: " + data[0].answer;
-               $("#output").html(temp);
+               //console.log(data);
+               var out = $("#output");
+               var qnum = Math.floor(Math.random()*dificulty);
+               console.log(qnum);
+               console.log(data[qnum]);
+               var temptxt = "question: " + data[qnum].question + "";
+               for(var i = 0; i < data.length;i++){
+                    temptxt += "<br><button id=qustion_" + i + ">" + data[i].answer + "</button>"
+               }
+               out.html(temptxt);
+
+
+               for(var i = 0; i < data.length;i++){
+                    if(i === qnum){
+                         $("#qustion_" + i).click(function(){
+                              $("#output").html("question: " + data[qnum].question + "<br>" + "Correct! good job<br>dificulty: " + dificulty);
+                              dificulty += 1;
+                         });
+                    } else{
+                         $("#qustion_" + i).click(function(){
+                              dificulty = 2;
+                              $("#output").html("question: " + data[qnum].question + "<br>" + "Sorry, the answer was " + data[qnum].answer + ", please try again.<br>dificulty: " + dificulty);
+                         });
+                    }
+               }
+               //var temp = "question: " + data[0].question + "<br><br>answer: " + data[0].answer;
+               //out.html(temp);
           },
           error: function (jqXHR, textStatus, errorThrown) {
               info = null;
@@ -30,3 +54,4 @@ function getData() {
 
 var button=$("#activate");
 button.click(getData);
+getData();
